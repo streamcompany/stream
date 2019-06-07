c372f5aef0e9ced61427d0374b1b01b1a1fa80dc
streamcompany <47898498+streamcompany@users.noreply.github.com>
2019-06-07 18:05:29 +0100

    Add files via upload

--- search.html/assets/theme/js/script.js
+++ search.html/assets/theme/js/script.js
@@ -1077,4 +1077,4 @@
         });
     }
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.10.0';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="/">Adrenaline has been destroyed by an element editor. Click this link to repair it. You will be taken to the host domain.</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
